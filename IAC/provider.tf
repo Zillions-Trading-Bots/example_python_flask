4ca0391b959aad23fee561849a0657d248726159
Airton Finoti <afinoti@users.noreply.github.com>
2022-04-13 16:01:14 -0300

    module

--- IAC/provider.tf
+++ IAC/provider.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "4.9.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.1.2"
-    }
   }
   required_version = ">= 1.1.0"
 
